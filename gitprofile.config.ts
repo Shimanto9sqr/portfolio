39beecbb04fc608357a95b00867692e5ff2d7467
Mazharul Islam Shimanto <119039604+Shimanto9sqr@users.noreply.github.com>
2025-10-22 00:53:13 +0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,20 +33,20 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Offline Password Vault',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'A offline encrypted password vault with feature like strong password generator',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://drive.google.com/file/d/1Vm1qzMxIiK71E9MVf6kWg2jAGiWWB6Nk/view?usp=drive_link',
+          link: '',
         },
         {
-          title: 'Project Name',
+          title: 'InfoRuet',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'A information portal for RUET where Faculty info, notices and university info centralized',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            '',
+          link: '',
         },
       ],
     },
